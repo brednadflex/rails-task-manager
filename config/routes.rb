51636bec27c47701cf369df2d724f831919ab0bc
Bernard Haas <barney.haas@gmail.com>
2021-05-18 15:59:03 +0200

    Task manager working without styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  # read all tasks
+  get '/tasks', to: 'tasks#index'
+  # create task
+  get "/tasks/new", to: "tasks#new" # display the form
+  # read one task
+  get "/tasks/:id", to: "tasks#show", as: :task
+
+  post '/tasks', to: 'tasks#create'
+
+  # update a task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # delete
+  delete '/tasks/:id', to: 'tasks#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources: tasks
 end
